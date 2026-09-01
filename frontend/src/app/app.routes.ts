c32bd3cb5f22273b367ee3534f485385fbaddb08
rgodinez-2022107 <rgodinez-2022107@kinal.edu.gt>
2026-09-01 17:58:21 -0600

    feat: definir las rutas y proteccion de la aplicacion

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+  },
+  { path: '**', redirectTo: '' },
+];
